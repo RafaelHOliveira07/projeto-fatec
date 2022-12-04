92a980f3ce80c0b92085c3f9dfd64362b71fe661
RafaelHOliveira07 <107508664+RafaelHOliveira07@users.noreply.github.com>
2022-12-04 12:01:20 -0300

    OK

--- arquivos/professor/js/Nav.js
+++ arquivos/professor/js/Nav.js
@@ -35,3 +35,49 @@ function reveal(){
     }
   }
 }
+var $input    = document.getElementById('input-file'),
+    $fileName = document.getElementById('file-name');
+    
+
+$input.addEventListener('change', function(){
+  $fileName.textContent = this.value;
+});
+
+var $input    = document.getElementById('input-file1'),
+    $fileName1 = document.getElementById('file-name1');
+    
+
+$input.addEventListener('change', function(){
+  $fileName1.textContent = this.value;
+});
+
+var $input    = document.getElementById('input-file2'),
+    $fileName2 = document.getElementById('file-name2');
+    
+
+$input.addEventListener('change', function(){
+  $fileName2.textContent = this.value;
+});
+
+var $input    = document.getElementById('input-file3'),
+    $fileName3 = document.getElementById('file-name3');
+    
+
+$input.addEventListener('change', function(){
+  $fileName3.textContent = this.value;
+});
+
+var $input    = document.getElementById('input-file4'),
+    $fileName4 = document.getElementById('file-name4');
+    
+
+$input.addEventListener('change', function(){
+  $fileName4.textContent = this.value;
+});
+var $input    = document.getElementById('input-file5'),
+    $fileName5 = document.getElementById('file-name5');
+    
+
+$input.addEventListener('change', function(){
+  $fileName5.textContent = this.value;
+});
