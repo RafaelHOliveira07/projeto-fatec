1740b8d33412bb358cba2e2e4e05e98de388bff3
Rafael H Oliveira <107508664+RafaelHOliveira07@users.noreply.github.com>
2023-05-12 21:23:30 -0300

    att da area adm e organização de pastas
    Pastas Organizadas para maior entendimento de toda a equipe, e também relinkando as folhas de estilo para que continuem funcionando normalmento.

---
+++ arquivos/adm_cord/adm/js/Nav.js
@@ -0,0 +1,7 @@
+
+var $input    = document.getElementById('input-file'),
+    $fileName = document.getElementById('file-name');
+
+$input.addEventListener('change', function(){
+  $fileName.textContent = this.value;
+});
